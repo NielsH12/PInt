7b256a03b940f456b87e7a67ce09ca1cf921483c
JensBAndersen <32388848+JensBAndersen@users.noreply.github.com>
2019-02-13 18:17:52 +0100

    Create database script for sql

---
+++ StorageManagement/src/main/resources/CreateDataBase.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Box (
+                       owner varchar(225),
+                       ID int,
+                       posX varchar(255),
+                       posY varchar(255)
+);
