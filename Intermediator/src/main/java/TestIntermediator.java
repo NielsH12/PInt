feef2d49160ccb2b07c25dbfe5a1071e28f5709c
JensBAndersen <32388848+JensBAndersen@users.noreply.github.com>
2019-02-14 12:32:31 +0100

    Added BoxHandler

--- Intermediator/src/main/java/TestIntermediator.java
+++ Intermediator/src/main/java/TestIntermediator.java
@@ -2,5 +2,9 @@ public class TestIntermediator {
     public static void main(String[] args) {
 
         System.out.println("Intermediator");
+
+        BoxHandler bh = new BoxHandler();
+
+        System.out.println(bh.GetBoxInfoByID(2));
     }
 }
